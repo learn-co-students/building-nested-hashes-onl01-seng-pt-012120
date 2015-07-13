cf3c892347c51397f6439b5f50e7e1aafa8d9334
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-07-13 11:41:01 -0400

    remove solution

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,31 +1,11 @@
 def fourth_challenge
-#   epic_tragedy = {
-#    :montague => {
-#       :patriarch => {name: "Lord Montague", age: "53"},
-#       :matriarch => {name: "Lady Montague", age: "54"},
-#       :hero => {name: "Romeo", age: "15", status: "alive"},
-#       :hero_friends => [
-#         #your hashes here!
-#       ]
-#    }, 
-#    :capulet => {
-#       :patriarch => {name: "Lord Capulet", age: "50"},
-#       :matriarch => {name: "Lady Capulet", age: "51"},
-#       :heroine => {name: "Juliette", age: "15", status: "alive"},
-#       :heroine_friends => [
-#         #your hashes here!
-#       ]
-#    }
-# }
-
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-         {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Steven", age: "30", attitude: "confused"}
+        #your hashes here!
       ]
    }, 
    :capulet => {
@@ -33,10 +13,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliette", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
-          {name: "Nurse", age: "44", attitude: "worried"}
+        #your hashes here!
       ]
    }
 }
+
+  
 
 end
--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,43 +1,30 @@
 def third_challenge
-  # epic_tragedy = {
-  #  :montague => {
-  #     :patriarch => {
-  #       #your key/value pairs here
-  #       },
-  #     :matriarch => {
-  #       #your key/value pairs here
-  #       },
-  #     :hero => {
-  #       #your key/value pairs here
-  #       },
-  #     :hero_friends => []
-  #  }, 
-  #  :capulet => {
-  #     :patriarch => {
-  #       #your key/value pairs here
-  #       },
-  #     :matriarch => {
-  #       #your key/value pairs here
-  #       },
-  #     :heroine => {
-  #       #your key/value pairs here
-  #       },
-  #     :heroine_friends => []
-  #  }
-  # }
-
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :hero => {
+        #your key/value pairs here
+        },
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliette", age: "15", status: "alive"},
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :heroine => {
+        #your key/value pairs here
+        },
       :heroine_friends => []
    }
-}
+  }
+
+  
 end
