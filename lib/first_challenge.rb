1c7b51872e4391ca57c1e660eac075360edd1662
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-07-13 11:38:19 -0400

    made code along lab

---
+++ lib/first_challenge.rb
@@ -0,0 +1,4 @@
+def first_challenge
+  # epic_tragedy = {}
+	epic_tragedy = {:montague => {}, :capulet => {}}
+end
---
+++ lib/second_challenge.rb
@@ -0,0 +1,25 @@
+def second_challenge
+  # epic_tragedy = {
+  #   :montague => {
+  #     #your key/value pairs here!
+  #     }, 
+  #   :capulet => {
+  #     #your key/value pairs here!
+  #     }}
+
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   }, 
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+  }
+
+end
