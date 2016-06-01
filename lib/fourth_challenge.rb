808e43a4f55cb838f192a80629a60e7a812742df
samnags <sam.nagourney@gmail.com>
2016-06-01 12:59:28 -0400

    New solution for issue #20

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,25 +1,26 @@
 def fourth_challenge
 
-
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-         {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Steven", age: "30", attitude: "confused"}
+         {name: "Mercutio", age: "18", attitude: "hot-headed"},
+         {name: "Benvolio", age: "17", attitude: "worried"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliette", age: "15", status: "alive"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Steven", age: "30", attitude: "confused"},
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-}
+  }
+
+
 
 end
