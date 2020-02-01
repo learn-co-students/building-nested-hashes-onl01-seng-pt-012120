8f58d6d1d246c217a7e86e097d6df452caee54ae
Harley McDowell <hmcdowell95@gmail.com>
2020-02-01 21:47:53 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,11 +19,10 @@ def bonus
       ]
    }
   }
-
-  #code your solution here:
-
-  
-
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   epic_tragedy
 end
+
+
+
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+  }
 
   
 end
