72a82cd16280da94e03b5fd08360d619ee4fc318
Beth Ruff <beth.ruff2@gmail.com>
2020-02-02 17:42:58 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,6 +22,9 @@ def bonus
 
   #code your solution here:
 
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
+  puts epic_tragedy
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,27 +2,30 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
-        },
+        name: "Lord Montague", age: "53"
+      },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Montague", age: "54"
         },
       :hero => {
-        #your key/value pairs here
+        name: "Romeo", age: "15", status: "alive"
         },
-      :hero_friends => []
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Capulet", age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet", age: "51"
         },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliet", age: "15", status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}]
    }
   }
 
