35764f16be77a50d2ef6777ae7fb2164c9199f3d
antromo <anthonyroma741@gmail.com>
2020-02-03 11:05:21 -0500

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,19 +1,21 @@
 def fourth_challenge
-  epic_tragedy = {
+epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
--- spec/bonus_spec.rb
+++ spec/bonus_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/bonus.rb'
 
 describe "#bonus" do 
   # remove the 'x' from the 'xit' below to activate this test!
-	xit "uses the []= method to change the hero and heroine's status from alive to dead" do 
+	it "uses the []= method to change the hero and heroine's status from alive to dead" do 
     expect(bonus[:montague][:hero][:status]).to eq("dead")
     expect(bonus[:capulet][:heroine][:status]).to eq("dead")
     
