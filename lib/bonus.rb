3481acaee2b5bed2aa3196f7ef471f01d42a11f6
michael otero <michaelotero1118@gmail.com>
2020-02-03 05:43:12 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,58 +1,29 @@
-
+def bonus
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Steven", age: "30", attitude: "confused"}
+      ]
+   }, 
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
+   }
+  }
 
   #code your solution here:
 
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-def bonus
-
-  epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
-
-puts epic_tragedy
-
-#  =>
-
-{
-
-   :montague => {
-
-      :patriarch => {name: "Michael Jordan", age: "53"},
-
-      :matriarch => {name: "Lady Montague", age: "54"},
-
-      :hero => {name: "Romeo", age: "15", status: "alive"},
-
-      :hero_friends => [
-
-        {name: "Benvolio", age: "17", attitude: "worried"},
-
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}
-
-      ]
-
-   },
-
-   :capulet => {
-
-      :patriarch => {name: "Lord Capulet", age: "50"},
-
-      :matriarch => {name: "Lady Capulet", age: "51"},
-
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-
-      :heroine_friends => [
-
-        {name: "Steven", age: "30", attitude: "confused"},
-
-        {name: "Nurse", age: "44", attitude: "worried"}
-
-      ]
-
-   }
-
-}
-
   epic_tragedy
-
 end
