a4ae90df6a35ba6ab3a05ce974b59b70d99074ca
Marvin Reyes <reyemarv@gmail.com>
2020-02-04 00:27:10 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []
       }}
 
   
